12e42ad523c143a627dc19689a2b62d9fc44c12b
janakiramgowni <116260751+janakiramgowni@users.noreply.github.com>
2022-10-25 10:28:11 +0530

    added comment in first.py

--- first.py
+++ first.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 import time
-
+# initial
 driver = webdriver.Chrome("C:\chromedriver\chromedriver_win32\chromedriver")
 driver.get("https://www.facebook.com/")
 time.sleep(3)
